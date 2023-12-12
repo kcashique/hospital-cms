4089bde0675eb66f9aa87cef0b81c29c7f56eb03
kcashique <ashiquekc97@gmail.com>
2023-12-12 18:14:20 +0530

    2 - routing demo data testing

--- modules/routes.py
+++ modules/routes.py
@@ -3,3 +3,7 @@ from werkzeug.wrappers import Response
 def index(self):
     data = 'hello World!'
     return Response(data)
+
+def doctors(self):
+    data = str(["ashique", "mubeen", "shanib","shabeeb"])
+    return Response(data)
--- server.py
+++ server.py
@@ -2,14 +2,15 @@ from werkzeug import Response, Request
 from werkzeug.routing import Rule, Map
 from werkzeug.exceptions import NotFound
 
-from modules.routes import index
+from modules.routes import index, doctors
 
 
 
 class HospitalSystem():
     def __init__(self):
         self.url_map=Map([
-            Rule('/', endpoint='index')
+            Rule('/', endpoint='index'),
+            Rule('/doctors', endpoint='doctors')
         ])
     
     def dispatch_request(self, request):
@@ -22,10 +23,12 @@ class HospitalSystem():
     
     def on_index(self, request):
         return index(request)
-
+    
+    def on_doctors(self, request):
+        return doctors(request)
 
     
-def app(environ, start_response):
+def application(environ, start_response):
     request = Request(environ)
     hospital_app = HospitalSystem()
     response=hospital_app.dispatch_request(request)
