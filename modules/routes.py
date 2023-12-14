40628d52567fbf84f400d8488f9ba796bed597da
kcashique <ashiquekc97@gmail.com>
2023-12-14 19:37:36 +0530

    4 - db_config tested and success.

--- modules/routes.py
+++ modules/routes.py
@@ -21,30 +21,22 @@ def index(self):
 
 
 def doctors(self):
-    students = [
-        {"name": "Sandrine", "score": 100},
-        {"name": "Gergeley", "score": 87},
-        {"name": "Frieda", "score": 92},
-        {"name": "Fritz", "score": 40},
-        {"name": "Sirius", "score": 75},
-    ]
     # Open a cursor to perform database operations
     cur = db_conn.cursor()
-    doctors_list =[]
-    result =cur.execute("SELECT doctor_name, fee FROM doctor_tbl;")
+    result_list =[]
+    cur.execute("SELECT doctor_name, fee FROM doctor_tbl;") # the excution qurry
+    result = cur.fetchall() #fetch all detials and add to result
     if result is not None:
         for item in result:
-            doctors_list.append(item)
+            result_list.append(item)
     else:
-        print ("check db or db_connection is not configured properly")
+        print ("check db_connection is configured properly")
     cur.close()
-    print (doctors_list)
-    max_score = 100
-    test_name = "Python Challenge"
+    doctors_list = [{"doctor_name":name, "fee":value} for name, value in result_list]
+    test_name = "Hospitel Management System"
     context={
-        "students":students,
         "test_name":test_name,
-        "max_score":max_score
+        "doctors_list":doctors_list
     }
     template = environment.get_template("result.html")
     return Response(template.render(context), content_type='text/html')
