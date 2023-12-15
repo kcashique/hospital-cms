5e2d74a19e5583197ddc11eb50665a007300865a
kcashique <ashiquekc97@gmail.com>
2023-12-15 23:38:22 +0530

    5 - models testing in progress

--- modules/routes.py
+++ modules/routes.py
@@ -3,6 +3,7 @@ from werkzeug.wrappers import Response
 from jinja2 import Environment, FileSystemLoader
 
 from modules.db_config import db_conn
+from modules.models import Doctor
 
 
 
@@ -24,7 +25,7 @@ def doctors(self):
     # Open a cursor to perform database operations
     cur = db_conn.cursor()
     result_list =[]
-    cur.execute("SELECT doctor_name, fee FROM doctor_tbl;") # the excution qurry
+    cur.execute("SELECT * FROM doctor_tbl;") # the excution qurry
     result = cur.fetchall() #fetch all detials and add to result
     if result is not None:
         for item in result:
@@ -32,7 +33,16 @@ def doctors(self):
     else:
         print ("check db_connection is configured properly")
     cur.close()
-    doctors_list = [{"doctor_name":name, "fee":value} for name, value in result_list]
+    # doctors_list = [{"doctor_name":name, "fee":value} for name, value in result_list]
+    doctors_list = [
+        Doctor(doctor_id=row[0], doctor_name=row[1], speciality=row[2], fee=row[3], scheduled_time=row[4], entry_date=row[5])
+        for row in result
+    ]
+    print (doctors_list)
+    # showing attribute error
+    response_data = "\n".join([f"{doctor.doctor_name}: {doctor.fee}" for doctor in doctors_list])
+    print (response_data)
+
     test_name = "Hospitel Management System"
     context={
         "test_name":test_name,
