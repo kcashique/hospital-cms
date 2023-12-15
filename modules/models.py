0d71bc4dbbf0a57ecabe12cd960f1ff83a31d7a1
kcashique <ashiquekc97@gmail.com>
2023-12-15 22:36:52 +0530

    sample doctor model created need to test

--- modules/models.py
+++ modules/models.py
@@ -1,5 +1,8 @@
-class Doctor():
-    def __init__():
+class Doctor:
+    def __init__(self, doctor_id, doctor_name, speciality, fee, scheduled_time, entry_date):
+        doctor_id = doctor_id
         doctor_name= doctor_name
-        
-
+        speciality = speciality
+        fee = fee
+        scheduled_time =scheduled_time
+        entry_date = entry_date
