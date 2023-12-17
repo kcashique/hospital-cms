2fc5fb9083b29a004fb311fbda1e8bee54a73fbb
kcashique <ashiquekc97@gmail.com>
2023-12-17 16:42:34 +0530

    basic exception handler implimented and tested. need to modify

--- modules/routes.py
+++ modules/routes.py
@@ -23,33 +23,45 @@ def index(self):
 
 def doctors(self, doctor_id):
     # Open a cursor to perform database operations
-    if doctor_id is None:
-        cur = db_conn.cursor()
-        cur.execute("SELECT doctor_id, doctor_name, fee  FROM doctor_tbl;") # the excution qurry
-        result = cur.fetchall() #fetch all detials and add to result
-        if result is not None:
-            pass
-        else:
-            print ("check db_connection is configured properly or db is none")
-        cur.close()
-        doctors_list = [{"doctor_id":row[0],  "doctor_name":row[1], "fee":row[2]} for row in result]
-        print (doctor_id)
-    if doctor_id is not None:
-        cur = db_conn.cursor()
-        cur.execute("SELECT doctor_id, doctor_name, fee  FROM doctor_tbl WHERE doctor_id=(%s);",(str(doctor_id))) # the excution qurry
-        result = cur.fetchall() #fetch all detials and add to result
-        if result is not None:
-            pass
-        else:
-            print ("check db_connection is configured properly or db is none")
-        cur.close()
-        doctors_list = [{"doctor_id":row[0],  "doctor_name":row[1], "fee":row[2]} for row in result]
-        print(doctors_list)
+    doctor_list=[]
+    try:
+        if doctor_id is None:
+            cur = db_conn.cursor()
+            cur.execute("SELECT doctor_id, doctor_name, fee  FROM doctor_tbl;") # the excution qurry
+            result = cur.fetchall() #fetch all detials and add to result
+            if result is not None:
+                pass
+            else:
+                print ("check db_connection is configured properly or db is none")
+            cur.close()
+            if len(result)==0:
+                print("No Data present in the given id")
+                raise ValueError(f'result_list={result}', "No Data present in database")
 
-    test_name = "Hospitel Management System"
-    context={
-        "test_name":test_name,
-        "doctors_list":doctors_list
-    }
-    template = environment.get_template("result.html")
-    return Response(template.render(context), content_type='text/html')                 
+            else:
+                    doctors_list = [{"doctor_id":row[0],  "doctor_name":row[1], "fee":row[2]} for row in result]
+        if doctor_id is not None:
+            cur = db_conn.cursor()
+            cur.execute("SELECT doctor_id, doctor_name, fee  FROM doctor_tbl WHERE doctor_id=(%s);",(str(doctor_id))) # the excution qurry
+            result = cur.fetchall() #fetch all detials and add to result
+            if result is not None:
+                pass
+            else:
+                print ("check db_connection is configured properly or db is none")
+            cur.close()
+            if len(result)==0:
+                print("No Data present in the given id")
+                raise ValueError(f'result_list={result}', "No Data present in the given id")
+            else:
+                doctors_list = [{"doctor_id":row[0],  "doctor_name":row[1], "fee":row[2]} for row in result]
+        test_name = "Hospitel Management System"
+        context={
+            "test_name":test_name,
+            "doctors_list":doctors_list
+        }
+        template = environment.get_template("result.html")
+        return Response(template.render(context), content_type='text/html')
+    except ValueError as err:
+        print (f"Data_Error :{err}")
+        resp= f"Data_Error :{err}"
+        return Response(resp, content_type='text/html', status=404)
